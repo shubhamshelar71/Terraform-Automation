d3920d23bd92e3517bf030a935b6ca6b374ecf63
shubhamshelar71 <shubhamshelar71@gmail.com>
2025-08-07 12:01:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,6 +47,7 @@ resource "aws_instance" "myFirstInstance" {
   key_name = var.key_name
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
+  subnet_id = data.aws_subnet.example.id
   tags= {
     Name = var.tag_name
   }
