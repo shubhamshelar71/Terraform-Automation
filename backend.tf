1c388862e19525bbf7b191a2ea23e836f56ea4fc
shubhamshelar71 <shubhamshelar71@gmail.com>
2025-11-18 14:31:06 +0530

    Update S3 bucket name in backend configuration

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-sample-batch-aws-devops-shubham-28"
+    bucket = "mydev-project-terraform-sample-batch-aws-devops-shubham-28-2444"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
+
